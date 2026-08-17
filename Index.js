af0c3058ccae99135b1ab3a6a311cbe23b0879f2
neddmont <vovakrasivih@gmail.com>
2026-08-17 17:56:02 +0300

    Испаравлен Content-Type

--- Index.js
+++ Index.js
@@ -72,8 +72,11 @@ if (loginForm) {
         try {
             const response = await fetch(`${API_URL}/auth/login`, {
                 method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: new URLSearchParams({ username: email, password })
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
             });
 
             const data = await response.json();
